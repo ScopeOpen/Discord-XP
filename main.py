343956e53d59b4d08a4f585da9dbfd6768bfe3c7
Sc00pe <github.scope.open@gmail.com>
2021-12-15 20:35:42 +1100

    Current Version Of DISCORD-XP
    Enjoy!

---
+++ main.py
@@ -0,0 +1,52 @@
+import discum
+import os
+import ctypes
+from colorama import Fore, Style
+import platform
+from  cv2 
+import glob
+
+ctypes.windll.kernel32.SetConsoleTitleW(f"Discord XP [{platform.system()}]")
+
+sent = 0
+os = os.system
+b = Style.BRIGHT
+
+
+
+
+
+__version__ = 1
+
+
+
+
+phrases = [
+  
+]
+
+channels = [
+
+]
+
+memechannel = [
+
+]
+
+xptime = ['', '']
+
+
+def main():
+
+    FirstMessage = f"""
+
+
+
+    """   
+
+if __name__ == '__main__':
+    main()
+
+
+
+
