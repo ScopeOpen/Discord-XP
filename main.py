6bc46315ea601fadb590ea7657bf4413ed122b5f
Sc00pe <github.scope.open@gmail.com>
2021-12-15 20:45:34 +1100

    Ascii Fixing 1.0

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os
 import ctypes
 from colorama import Fore, Style
 import platform
-from  cv2 
+import cv2 
 import glob
+import re
 
 ctypes.windll.kernel32.SetConsoleTitleW(f"Discord XP [{platform.system()}]")
 
@@ -12,12 +13,15 @@ sent = 0
 os = os.system
 b = Style.BRIGHT
 
+pathfiles = 'path/to/files/'
+ext = ['png', 'jpg', 'gif', 'mp4']
+files = []
+[files.extend(glob.glob(pathfiles + '*.' + e)) for e in ext]
 
-
-
+color1 = b+Fore.RED
+color2 = b+Fore.WHITE
 
 __version__ = 1
-
 
 
 
@@ -29,20 +33,32 @@ channels = [
 
 ]
 
-memechannel = [
+memechannelS = [
 
 ]
 
-xptime = ['', '']
+xptime = ['TIME1', 'TIME2']
 
+
+memes = [cv2.imread(file) for file in files]
 
 def main():
 
-    FirstMessage = f"""
+    NeedReplace = f"""
+        
+██████╗ ██╗███████╗ ██████╗ ██████╗ ██████╗ ██████╗      ██╗  ██╗██████╗ 
+██╔══██╗██║██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔══██╗     ╚██╗██╔╝██╔══██╗
+██║  ██║██║███████╗██║     ██║   ██║██████╔╝██║  ██║█████╗╚███╔╝ ██████╔╝
+██║  ██║██║╚════██║██║     ██║   ██║██╔══██╗██║  ██║╚════╝██╔██╗ ██╔═══╝ 
+██████╔╝██║███████║╚██████╗╚██████╔╝██║  ██║██████╔╝     ██╔╝ ██╗██║     
+╚═════╝ ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝      ╚═╝  ╚═╝╚═╝     
+                                                                                  
 
 
+    """
 
-    """   
+
+    
 
 if __name__ == '__main__':
     main()
