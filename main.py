d3175fb444e6cf07466465df1677ddf084405968
Sc00pe <github.scope.open@gmail.com>
2021-12-16 19:46:10 +1100

    Txt File Reading

--- main.py
+++ main.py
@@ -6,42 +6,78 @@ import platform
 import cv2 
 import glob
 import re
+import random
 
+# CMD PROMPT TOP WINDOW
 ctypes.windll.kernel32.SetConsoleTitleW(f"Discord XP [{platform.system()}]")
 
+# RANDOM STUFF
 sent = 0
 os = os.system
 b = Style.BRIGHT
 
+# MEME STUFF
 pathfiles = 'path/to/files/'
 ext = ['png', 'jpg', 'gif', 'mp4']
 files = []
 [files.extend(glob.glob(pathfiles + '*.' + e)) for e in ext]
 
+# COLOR DEFINITIONS
 color1 = b+Fore.GREEN
 color2 = b+Fore.WHITE
 color3 = b+Fore.GREEN
 
+# VERSION OF APPLICATION
 __version__ = 1
 
-
-
-phrases = [
-  
-]
-
-channels = [
-
-]
-
-memechannelS = [
-
-]
-
+# TIMES WHERE MESSAGES WILL BE SENT
 xptime = ['TIME1', 'TIME2']
 
-
+# SHOWING ALL FILES IN /memes/ DIRECTORY
 memes = [cv2.imread(file) for file in files]
+
+# RESETING OS DEFINITION
+import os
+
+# ALL OUT TXT FILES
+phrasesFile = "phrases.txt"
+channelsFile = "channels.txt"
+memechannelFile = "memechannel.txt"
+
+# GETTING INFO OFF THOSE TXT FILES
+
+# PHRASES TXT FILE READING
+if os.path.isfile(phrasesFile):
+    OpenPhrases = open(phrasesFile, "r")
+    lines1 = OpenPhrases.read()
+    PhrasesContents = lines1.splitlines()
+    OpenPhrases.close()
+
+# ALL CHANNELS TXT FILE READING
+if os.path.isfile(channelsFile):
+    OpenChannel = open(channelsFile, "r")
+    lines2 = OpenChannel.read()
+    ChannelContents = lines2.splitlines()
+    OpenChannel.close()
+
+# MEME CHANNEL TXT FILE READING
+if os.path.isfile(memechannelFile):
+    OpenMeme = open(memechannelFile, "r")
+    lines3 = OpenMeme.read()
+    MemeContents = lines3.splitlines()
+    OpenMeme.close()
+
+
+# ALL CONTENTS OF TXT FILES
+phrases = PhrasesContents
+memeChannel = MemeContents
+phrases = PhrasesContents
+
+# JUST TESTING IF READING FILES WORKS
+print(PhrasesContents)
+print(ChannelContents)
+print(MemeContents)
+
 
 def main():
 
@@ -70,8 +106,9 @@ def main():
     
 
 
-    
 
 if __name__ == '__main__':
     main()
 
+
+ 
